0dc6008d50ffbcac7ef89fe7f0d5a7358bcd6492
Florian Obst <f.obst@lw-systems.de>
2023-01-19 13:32:11 +0100

    feature: add SSL functionality to project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.glassfish.grizzly.ssl.SSLContextConfigurator;
+import org.glassfish.grizzly.ssl.SSLEngineConfigurator;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -13,12 +16,22 @@ import org.glassfish.grizzly.http.server.HttpServer;
 
 public class Main {
 
-    private static final URI BASE_URI = URI.create("http://localhost:8080/rest/");
+    private static final String KEYSTORE_SERVER_FILE = "src/main/resources/cert.p12";
+    private static final String KEYSTORE_SERVER_PWD = "pass";
+
+    private static final URI BASE_URI = URI.create("https://127.0.0.1:8010/rest/");
     public static final String ROOT_PATH = "upload";
 
     public static void main(String[] args) {
         try {
             System.out.println("Benno Rest upload file example");
+
+            SSLContextConfigurator sslContext = new SSLContextConfigurator();
+
+            // set up security context
+            sslContext.setKeyStoreFile(new File(KEYSTORE_SERVER_FILE).getAbsolutePath()); // contains server keypair
+            sslContext.setKeyStorePass(KEYSTORE_SERVER_PWD);
+
 
             /**
              * Required for MULTIPART_FORM_DATA. Without the package the server doesn't start
@@ -32,7 +45,12 @@ public class Main {
 
 
             // Start server
-            final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, resourceConfig, false);
+            final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(
+                    BASE_URI,
+                    resourceConfig,
+                    true,
+                    new SSLEngineConfigurator(sslContext).setClientMode(false).setNeedClientAuth(false)
+            );
             Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
                 @Override
                 public void run() {
